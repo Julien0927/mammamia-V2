4ddbbb56fbe327e78dd98653a6fc420374c0079f
Julien Varachas <varachasjulien@gmail.com>
2024-08-20 09:47:30 +0200

    MAJ page Menu

--- menu.php
+++ menu.php
@@ -2,17 +2,50 @@
 
 require_once ('header.php');
 ?>
-<ul class="nav justify-content-center">
-  <li class="nav-item">
-    <a class="nav-link active text-black" aria-current="page" href="#">PATES</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link text-black" href="#">PIZZAS</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link text-black" href="#">MENU DU JOUR</a>
-  </li>
-</ul>
+<div class=" navMenu">
+  <div>
+    <h3>
+      <a class="btnPasta mx-auto" data-bs-toggle="collapse" href="#collapsePasta" role="button" aria-expanded="false" aria-controls="collapseExample">
+      PATES
+      </a>
+    </h3>
+    <div class="collapse" id="collapsePasta">
+      <div class="card card-body">
+        Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+      </div>
+    </div>
+  <div>
+  <div>
+    <h3>
+      <a  class="btnPizza mx-auto" data-bs-toggle="collapse" href="#collapsePizza" role="button" aria-expanded="false" aria-controls="collapseExample">
+      PIZZAS
+      </a>
+    </h3>
+    <div class="collapse" id="collapsePizza">
+      <div class="card card-body">
+        <ul>
+          <li>Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.</li>
+          <li>Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.</li>
+          <li>Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.</li>
+          <li>Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.</li>
+          <li>Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.</li>
+          <li>Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.</li>
+        </ul>
+      </div>
+    </div>
+  <div>
+  <div>
+    <h3>
+      <a class="btnMenu mx-auto" data-bs-toggle="collapse" href="#collapseMenu" role="button" aria-expanded="false" aria-controls="collapseExample">
+      MENU DU JOUR
+      </a>
+    </h3>
+    <div class="collapse" id="collapseMenu">
+      <div class="card card-body">
+        Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+      </div>
+    </div>
+</div>
 <?php
 require_once ('templates/footer.php');
 ?>
